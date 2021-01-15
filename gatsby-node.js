143d91307d6db790d08cf6cfaee6ec2701765eb0
Alex Bea <abea@mailbox.org>
2021-01-15 12:55:48 -0600

    Removes starter comment

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,14 +4,6 @@
  *
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
-// You can delete this file if you're not using it
-
-/**
- * You can uncomment the following line to verify that
- * your plugin is being loaded in your site.
- *
- * See: https://www.gatsbyjs.com/docs/creating-a-local-plugin/#developing-a-local-plugin-that-is-outside-your-project
- */
 const fetch = require('node-fetch').default;
 const camelcase = require('camelcase').default;
 
